5b9fa6ec112578d12ddc7ebd5fc2286077ed3f5c
muhammedtanriverdi <muhammedtanriverdi@gmail.com>
2013-10-22 17:49:31 +0300

    Commit
    First Commit

---
+++ MultipleImageResizer/MainForm.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace MultipleImageResizer
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fbdSelectImageFolder = new System.Windows.Forms.FolderBrowserDialog();
+            this.btnSelectFolder = new System.Windows.Forms.Button();
+            this.tbWidth = new System.Windows.Forms.TextBox();
+            this.tbHeight = new System.Windows.Forms.TextBox();
+            this.gbExportOptions = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnExportFiles = new System.Windows.Forms.Button();
+            this.pbExportFiles = new System.Windows.Forms.ProgressBar();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.tssl_Status = new System.Windows.Forms.ToolStripStatusLabel();
+            this.bwExportFiles = new System.ComponentModel.BackgroundWorker();
+            this.cbMaintainAspectRatio = new System.Windows.Forms.CheckBox();
+            this.rtbSelectedFolder = new System.Windows.Forms.RichTextBox();
+            this.gbExportOptions.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnSelectFolder
+            // 
+            this.btnSelectFolder.Location = new System.Drawing.Point(13, 13);
+            this.btnSelectFolder.Name = "btnSelectFolder";
+            this.btnSelectFolder.Size = new System.Drawing.Size(260, 23);
+            this.btnSelectFolder.TabIndex = 0;
+            this.btnSelectFolder.Text = "Select Folder";
+            this.btnSelectFolder.UseVisualStyleBackColor = true;
+            this.btnSelectFolder.Click += new System.EventHandler(this.btnSelectFolder_Click);
+            // 
+            // tbWidth
+            // 
+            this.tbWidth.Location = new System.Drawing.Point(47, 28);
+            this.tbWidth.Name = "tbWidth";
+            this.tbWidth.Size = new System.Drawing.Size(42, 20);
+            this.tbWidth.TabIndex = 1;
+            this.tbWidth.Text = "200";
+            this.tbWidth.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // tbHeight
+            // 
+            this.tbHeight.Location = new System.Drawing.Point(47, 54);
+            this.tbHeight.Name = "tbHeight";
+            this.tbHeight.Size = new System.Drawing.Size(42, 20);
+            this.tbHeight.TabIndex = 2;
+            this.tbHeight.Text = "200";
+            this.tbHeight.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // gbExportOptions
+            // 
+            this.gbExportOptions.Controls.Add(this.cbMaintainAspectRatio);
+            this.gbExportOptions.Controls.Add(this.pbExportFiles);
+            this.gbExportOptions.Controls.Add(this.btnExportFiles);
+            this.gbExportOptions.Controls.Add(this.label2);
+            this.gbExportOptions.Controls.Add(this.label1);
+            this.gbExportOptions.Controls.Add(this.tbWidth);
+            this.gbExportOptions.Controls.Add(this.tbHeight);
+            this.gbExportOptions.Enabled = false;
+            this.gbExportOptions.Location = new System.Drawing.Point(13, 119);
+            this.gbExportOptions.Name = "gbExportOptions";
+            this.gbExportOptions.Size = new System.Drawing.Size(260, 167);
+            this.gbExportOptions.TabIndex = 3;
+            this.gbExportOptions.TabStop = false;
+            this.gbExportOptions.Text = "Export";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "width";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "height";
+            // 
+            // btnExportFiles
+            // 
+            this.btnExportFiles.Location = new System.Drawing.Point(7, 108);
+            this.btnExportFiles.Name = "btnExportFiles";
+            this.btnExportFiles.Size = new System.Drawing.Size(247, 23);
+            this.btnExportFiles.TabIndex = 5;
+            this.btnExportFiles.Text = "Export Files";
+            this.btnExportFiles.UseVisualStyleBackColor = true;
+            this.btnExportFiles.Click += new System.EventHandler(this.btnExportFiles_Click);
+            // 
+            // pbExportFiles
+            // 
+            this.pbExportFiles.Location = new System.Drawing.Point(6, 137);
+            this.pbExportFiles.Name = "pbExportFiles";
+            this.pbExportFiles.Size = new System.Drawing.Size(248, 23);
+            this.pbExportFiles.TabIndex = 6;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tssl_Status});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 289);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(285, 22);
+            this.statusStrip1.TabIndex = 4;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // tssl_Status
+            // 
+            this.tssl_Status.Name = "tssl_Status";
+            this.tssl_Status.Size = new System.Drawing.Size(0, 17);
+            // 
+            // bwExportFiles
+            // 
+            this.bwExportFiles.WorkerReportsProgress = true;
+            this.bwExportFiles.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bwExportFiles_DoWork);
+            this.bwExportFiles.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.bwExportFiles_ProgressChanged);
+            this.bwExportFiles.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.bwExportFiles_RunWorkerCompleted);
+            // 
+            // cbMaintainAspectRatio
+            // 
+            this.cbMaintainAspectRatio.AutoSize = true;
+            this.cbMaintainAspectRatio.Location = new System.Drawing.Point(7, 85);
+            this.cbMaintainAspectRatio.Name = "cbMaintainAspectRatio";
+            this.cbMaintainAspectRatio.Size = new System.Drawing.Size(130, 17);
+            this.cbMaintainAspectRatio.TabIndex = 7;
+            this.cbMaintainAspectRatio.Text = "Maintain Aspect Ratio";
+            this.cbMaintainAspectRatio.UseVisualStyleBackColor = true;
+            // 
+            // rtbSelectedFolder
+            // 
+            this.rtbSelectedFolder.Enabled = false;
+            this.rtbSelectedFolder.Location = new System.Drawing.Point(13, 42);
+            this.rtbSelectedFolder.Name = "rtbSelectedFolder";
+            this.rtbSelectedFolder.Size = new System.Drawing.Size(260, 44);
+            this.rtbSelectedFolder.TabIndex = 6;
+            this.rtbSelectedFolder.Text = "";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(285, 311);
+            this.Controls.Add(this.rtbSelectedFolder);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.gbExportOptions);
+            this.Controls.Add(this.btnSelectFolder);
+            this.Name = "MainForm";
+            this.Text = "Multiple Image Resizer";
+            this.gbExportOptions.ResumeLayout(false);
+            this.gbExportOptions.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog fbdSelectImageFolder;
+        private System.Windows.Forms.Button btnSelectFolder;
+        private System.Windows.Forms.TextBox tbWidth;
+        private System.Windows.Forms.TextBox tbHeight;
+        private System.Windows.Forms.GroupBox gbExportOptions;
+        private System.Windows.Forms.Button btnExportFiles;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar pbExportFiles;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel tssl_Status;
+        private System.ComponentModel.BackgroundWorker bwExportFiles;
+        private System.Windows.Forms.CheckBox cbMaintainAspectRatio;
+        private System.Windows.Forms.RichTextBox rtbSelectedFolder;
+    }
+}
+
---
+++ MultipleImageResizer/MainForm.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MultipleImageResizer
+{
+    public partial class MainForm : Form
+    {
+        /// <summary>
+        /// Selected folder path
+        /// </summary>
+        private string folderPath;
+
+        /// <summary>
+        /// File list in selected folder
+        /// </summary>
+        private List<string> imageFilesList;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSelectFolder_Click(object sender, EventArgs e)
+        {
+            
+
+            // Control user select a folder
+            if (fbdSelectImageFolder.ShowDialog() == DialogResult.OK)
+            {
+                // Get the path of selected folder
+                folderPath = fbdSelectImageFolder.SelectedPath;
+
+                // Set the files name to the list
+                imageFilesList = Directory.GetFiles(folderPath, "*.*", SearchOption.AllDirectories).ToList();
+
+                // Write selected folder to textbox
+                rtbSelectedFolder.Text = folderPath;
+
+                // Enable export files button
+                gbExportOptions.Enabled = true;
+            }
+            else
+            {
+                // Set empty textbox of selected folder
+                rtbSelectedFolder.Text = "";
+
+                // Disable export files button
+                gbExportOptions.Enabled = false;
+            }
+        }
+
+
+        private void btnExportFiles_Click(object sender, EventArgs e)
+        {
+            pbExportFiles.Maximum = imageFilesList.Count;
+            // Start the background operations
+            bwExportFiles.RunWorkerAsync();
+        }
+
+        /// <summary>
+        /// Set status stript text
+        /// </summary>
+        /// <param name="text">Status text</param>
+        private void SetStatusStripText(string text)
+        {
+            tssl_Status.Text = text;
+        }
+
+
+        /// <summary>
+        /// Method to resize, convert and save the image.
+        /// </summary>
+        /// <param name="image">Bitmap image.</param>
+        /// <param name="maxWidth">resize width.</param>
+        /// <param name="maxHeight">resize height.</param>
+        /// <param name="quality">quality setting value.</param>
+        /// <param name="filePath">file path.</param>      
+        public void Save(Bitmap image, int maxWidth, int maxHeight, int quality, string filePath)
+        {
+            // Get the image's original width and height
+            int originalWidth = image.Width;
+            int originalHeight = image.Height;
+
+            // To preserve the aspect ratio
+            float ratioX = (float)maxWidth / (float)originalWidth;
+            float ratioY = (float)maxHeight / (float)originalHeight;
+            float ratio = Math.Min(ratioX, ratioY);
+
+            // New width and height based on aspect ratio
+            int newWidth = (int)(originalWidth * ratio);
+            int newHeight = (int)(originalHeight * ratio);
+
+            // Convert other formats (including CMYK) to RGB.
+            Bitmap newImage;
+            if (cbMaintainAspectRatio.Checked)
+            {
+                newImage = new Bitmap(newWidth, newHeight, PixelFormat.Format24bppRgb);
+            }
+            else
+            {
+                newImage = new Bitmap(maxWidth, maxHeight, PixelFormat.Format24bppRgb);
+            }
+
+            // Draws the image in the specified size with quality mode set to HighQuality
+            using (Graphics graphics = Graphics.FromImage(newImage))
+            {
+                graphics.CompositingQuality = CompositingQuality.HighQuality;
+                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                graphics.SmoothingMode = SmoothingMode.HighQuality;
+
+                if (cbMaintainAspectRatio.Checked)
+                {
+                    graphics.DrawImage(image, 0, 0, newWidth, newHeight);
+                }
+                else
+                {
+                    graphics.DrawImage(image, 0, 0, maxWidth, maxHeight);
+                }
+            }
+
+            // Get an ImageCodecInfo object that represents the JPEG codec.
+            ImageCodecInfo imageCodecInfo = this.GetEncoderInfo(ImageFormat.Jpeg);
+
+            // Create an Encoder object for the Quality parameter.
+            Encoder encoder = Encoder.Quality;
+
+            // Create an EncoderParameters object. 
+            EncoderParameters encoderParameters = new EncoderParameters(1);
+
+            // Save the image as a JPEG file with quality level.
+            EncoderParameter encoderParameter = new EncoderParameter(encoder, quality);
+            encoderParameters.Param[0] = encoderParameter;
+            newImage.Save(filePath, imageCodecInfo, encoderParameters);
+        }
+
+        /// <summary>
+        /// Method to get encoder infor for given image format.
+        /// </summary>
+        /// <param name="format">Image format</param>
+        /// <returns>image codec info.</returns>
+        private ImageCodecInfo GetEncoderInfo(ImageFormat format)
+        {
+            return ImageCodecInfo.GetImageDecoders().SingleOrDefault(c => c.FormatID == format.Guid);
+        }
+
+        private void bwExportFiles_DoWork(object sender, DoWorkEventArgs e)
+        {
+            Bitmap newImage;
+
+            string subPath = folderPath + "/small/";
+
+            bool isExists = System.IO.Directory.Exists(subPath);
+
+            if (!isExists)
+                System.IO.Directory.CreateDirectory(subPath);
+
+            int filesCount = imageFilesList.Count;
+            int i = 0;
+            
+            foreach (string fileName in imageFilesList)
+            {
+                try
+                {
+                    Console.WriteLine(subPath + fileName);
+                    newImage = new Bitmap(Image.FromFile(fileName));
+                    Save(newImage, Convert.ToInt16(tbWidth.Text), Convert.ToInt16(tbHeight.Text), 100, subPath + Path.GetFileName(fileName));
+                }
+                catch (Exception ex)
+                {
+                }
+
+                bwExportFiles.ReportProgress(i++);
+            }
+        }
+
+        private void bwExportFiles_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            pbExportFiles.Value = e.ProgressPercentage;
+        }
+
+        private void bwExportFiles_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            SetStatusStripText("All image files export succesfully");
+        }
+    }
+
+}
